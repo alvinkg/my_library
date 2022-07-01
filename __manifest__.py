687d9ed3775b5ce033a5aafea55e6bb83e870fc7
alvinkg <lusnalvin@yahoo.com>
2022-07-01 18:51:08 +0800

    Adding access security Fixes #11

--- __manifest__.py
+++ __manifest__.py
@@ -7,7 +7,11 @@
     'category': 'Uncategorized',
     'version': '14.0.1',
     'depends': ['base'],
-    #'data': ['views/views.xml'],
+    'data': [
+        'views/library_book.xml',
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        ],
     #'demo': ['demo.xml'],
     'application': True,
     'sequence': 0,
